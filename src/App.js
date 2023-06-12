8ec632559e73bea77f752adecec7c65eab214dfb
Zaidi <zaidiahsan.r@gmail.com>
2023-06-11 19:38:34 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -4,25 +4,59 @@ import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-]);
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile"
 
 function App() {
+
+  const Layout = () => {
     return (
       <div>
-        <RouterProvider router={router} />
+        <Navbar />
+        <div style={{display:"flex"}}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
       </div>
-    );
+    )
   }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
   
-  export default App;
+export default App;
