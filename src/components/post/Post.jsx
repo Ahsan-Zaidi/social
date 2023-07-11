197d85122cc1327a66758bc80d0517e0dfe72218
Zaidi <zaidiahsan.r@gmail.com>
2023-07-11 14:24:37 -0500

    comments component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,8 +5,12 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 const Post = ({post}) => {
+
+    const [commentOpen, setCommentOpen] = useState(false)
 
     const liked = false;
 
@@ -34,7 +38,7 @@ const Post = ({post}) => {
                         {liked ? <FavoriteOutlinedIcon/> : <FavoriteBorderOutlinedIcon/>}
                         12 likes
                     </div>
-                    <div className="item">
+                    <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
                         <TextsmsOutlinedIcon />
                         5 comments
                     </div>
@@ -43,6 +47,7 @@ const Post = ({post}) => {
                         Share
                     </div>
                 </div>
+                {commentOpen && <Comments />}
             </div>
         </div>
     )
